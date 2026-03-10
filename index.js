5876f07ddf40f59ce1efa02dd806ff9da72866fb
Oleksandr Volskyi <136353538+OleksandrVolsky@users.noreply.github.com>
2026-03-10 16:50:15 +0200

    feat: add example usage of the library in index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const my_library = require("./File_Library") // імпорт бібліотеки 
+// Приклад...
+
+const my_library = require("./File_Library") // імпортуємо бібліотеку
+
+my_library.newFolder("NewFolder") // створюємо попку
+
+setTimeout(() => {
+    my_library.createFile("./NewFolder/NewFile.txt") // створюємо файл в цій папці
+
+    setTimeout(() => {
+        my_library.appendFile("./NewFolder/NewFile.txt", "Hello world") // записуємо текст в файл
+    }, 200)
+
+    setTimeout(() => {
+        my_library.readFile("./NewFolder/NewFile.txt") // читаємо файл і виводемо текст в консоль
+    }, 200)
+}, 200)
